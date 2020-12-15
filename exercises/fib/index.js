db0f8f6bbe0f5797a054ec65c7fc82166036bc2b
YIRer <xeroonelove@gmail.com>
2020-12-15 17:45:04 +0900

    fibonacci

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,6 +8,51 @@
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+// function fib(n) {
+//   if (n === 1 || n === 2) {
+//     return 1;
+//   }
+//   const result = [0, 1];
+//   for (let i = 2; i <= n; i++) {
+//     const a = result[i - 1];
+//     const b = result[i - 2];
+
+//     result.push(a + b);
+//   }
+
+//   return result[result.length - 1];
+// }
+
+// use memoization
+function slowfib(n) {
+  if (n < 2) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
+function memoize(fn) {
+  const cache = {};
+  return (...args) => {
+    if (cache[args]) {
+      return cache[args];
+    }
+
+    const result = fn.apply(this, args);
+    cache[args] = result;
+    return result;
+  };
+}
+
+const fib = memoize(slowfib);
+
+// function fib(n) {
+//   if (n < 2) {
+//     return n;
+//   }
+
+//   return fib(n - 1) + fib(n - 2);
+// }
 
 module.exports = fib;
