5c7c534745aa6b09a110006855b6aac02779b80d
YIRer <xeroonelove@gmail.com>
2020-12-17 18:29:53 +0900

    levelWith

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,22 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  const arr = [root, "s"];
+  const levelWidth = [0];
+  while (arr.length > 1) {
+    const node = arr.shift();
+
+    if (node === "s") {
+      levelWidth.push(0);
+      arr.push("s");
+    } else {
+      levelWidth[levelWidth.length - 1]++;
+      arr.push(...node.children);
+    }
+  }
+
+  return levelWidth;
+}
 
 module.exports = levelWidth;
