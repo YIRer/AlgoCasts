88d16f0981088d858970371e1ddff70056acb186
YIRer <xeroonelove@gmail.com>
2020-12-21 20:43:43 +0900

    events and sorting

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,51 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+  constructor(data, left = null, right = null) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+
+  insertNode(node, newNode) {
+    if (node.data > newNode.data) {
+      if (node.left === null) {
+        node.left = newNode;
+      } else {
+        this.insertNode(node.left, newNode);
+      }
+    } else if (node.data < newNode.data) {
+      if (node.right === null) {
+        node.right = newNode;
+      } else {
+        this.insertNode(node.right, newNode);
+      }
+    }
+  }
+
+  insert(value) {
+    this.insertNode(this, new Node(value));
+  }
+
+  find(node, value) {
+    if (node === null) {
+      return null;
+    }
+
+    if (node.data > value) {
+      return this.find(node.left, value);
+    } else if (node.data < value) {
+      return this.find(node.right, value);
+    } else {
+      return node;
+    }
+  }
+
+  contains(value) {
+    return this.find(this, value);
+  }
+}
+
 
 module.exports = Node;
